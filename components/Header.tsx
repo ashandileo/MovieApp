068403431037e823e89cb1a977ee76f3aa2aedbc
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2023-02-05 01:38:05 +0700

    create header component

---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="py-[24px] w-full fixed z-[2]">
+      <div className="container flex items-center justify-between text-white">
+        <div className="flex items-center gap-[40px]">
+          <a>Movies</a>
+          <a>About</a>
+        </div>
+        <div>
+          <p>GoMovies</p>
+        </div>
+        <div>
+          <p>Search Movies</p>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
