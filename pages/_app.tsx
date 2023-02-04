8d07cf93d0baf4531b24e545f2bd1c4d33d4334d
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2023-02-05 01:38:11 +0700

    import header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Header from "../components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -15,6 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
         <title>Ashandi's XXI</title>
 
+        <Header />
         <Component {...pageProps} />
       </Head>
     </>
