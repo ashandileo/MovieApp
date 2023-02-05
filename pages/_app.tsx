fe3099b6323388b5a791504a0dc599ff0ab9a966
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2023-02-05 10:42:40 +0700

    change font to roboto

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,15 +10,15 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;500;700&family=Roboto:wght@300;400;500&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
           rel="stylesheet"
         />
 
         <title>Ashandi's XXI</title>
-
-        <Header />
-        <Component {...pageProps} />
       </Head>
+
+      <Header />
+      <Component {...pageProps} />
     </>
   );
 }
