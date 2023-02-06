acfbae38ebf57f3f497a01bf21de52d7ae8bcd5c
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2023-02-06 14:02:38 +0700

    test

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ module.exports = {
     API_HOST: "https://api.themoviedb.org/3",
     API_KEY: "cf6b012a8f4aa1d8f965f77016661aca",
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
